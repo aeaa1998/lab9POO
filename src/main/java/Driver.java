6af6bdabe7360096131313aab23fe09254c72276
Augusto Alonso <augustoalonso98@gmail.comn>
2019-11-03 16:36:11 -0600

    first commit version 2 ya funcional

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -7,7 +7,7 @@ public class Driver {
         String[] whoElse = {"a sus vidas", "al mundo"};
         components.add(new Predicate("trae", "bendiciones"));
         components.add(new Subject("El principio de abierto-cerrado (open-closed principle en inglés) "));
-//        components.add(new IndirectComplement("a sus programas", whoElse))
+        components.add(new IndirectComplement("a sus programas", whoElse));
         writer.setComponents(components);
         writer.writeComplements();
 
